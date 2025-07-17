f2499b7f71d511d9da48c209862a4eb37054d161
Rafael Guedes <rafaelmagalhaes1@live.com>
2025-07-17 17:01:57 -0300

    feat: url backend fixed to deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { Product, CreateProductRequest, UpdateProductRequest, ApiResponse } from '../types/product';
 
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'https://products-backend-production-a43e.up.railway.app';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
